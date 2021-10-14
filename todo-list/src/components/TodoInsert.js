9b934a7295b0008fc078218c86ad2a5a95e9cf04
younglys <youngly.dev@gmail.com>
2021-10-14 15:47:52 +0900

    Add: 항목 추가 기능-TodoInsert value 상태 관리하기 구현

--- todo-list/src/components/TodoInsert.js
+++ todo-list/src/components/TodoInsert.js
@@ -1,10 +1,21 @@
+import { useState, useCallback } from 'react';
 import { GrAdd } from 'react-icons/gr';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState();
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form className="toDoInsert">
-      <input placeholder="Write what you need to do" />
+      <input
+        placeholder="Write what you need to do"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <GrAdd />
       </button>
