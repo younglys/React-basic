62ce925fe9b47f817644ca134ba4448d8e2efc83
younglys <youngly.dev@gmail.com>
2021-10-14 17:29:23 +0900

    Add: 항목 추가 기능-TodoInsert에서 onSubmit 이벤트 설정하기 구현

--- todo-list/src/components/TodoInsert.js
+++ todo-list/src/components/TodoInsert.js
@@ -2,15 +2,24 @@ import { useState, useCallback } from 'react';
 import { GrAdd } from 'react-icons/gr';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState();
 
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="toDoInsert">
+    <form className="toDoInsert" onSubmit={onSubmit}>
       <input
         placeholder="Write what you need to do"
         value={value}
