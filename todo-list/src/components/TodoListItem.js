9b10ec24dd6a01459d7699aba7b11ab23946621a
younglys <youngly.dev@gmail.com>
2021-10-14 18:13:16 +0900

    Add: 체크 수정 기능-onToggle 이벤트 및 토글 함수 호출하기 구현

--- todo-list/src/components/TodoListItem.js
+++ todo-list/src/components/TodoListItem.js
@@ -7,12 +7,15 @@ import {
 import classnames from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
 
   return (
     <div className="toDoListItem">
-      <div className={classnames('checkbox', { checked })}>
+      <div
+        className={classnames('checkbox', { checked })}
+        onClick={() => onToggle(id)}
+      >
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
