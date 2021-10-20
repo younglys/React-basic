6835e415838472e6aeec8243001cbea69ffed786
younglys <youngly.dev@gmail.com>
2021-10-20 15:53:40 +0900

    Practice axios exam for newsapi

---
+++ news-viewer/src/newsapi_exam.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const res = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=f0ddb10c94cc44fbadbdb1b4042cee79',
+      );
+      setData(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          row={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
